981a477033b54765c73928fc94d132ee29520237
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-20 10:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(time) {
-  if {
-    
-  }
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
 }
 
 /* Write your implementation of displayMessage() */
